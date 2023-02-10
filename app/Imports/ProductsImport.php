dcae8912f891bdb52a4d242a5c4841068df32213
Adis <adisazizan@gmail.com>
2023-02-10 09:09:41 +0800

    Upload with new data

--- app/Imports/ProductsImport.php
+++ app/Imports/ProductsImport.php
@@ -2,7 +2,10 @@
 
 namespace App\Imports;
 
+use App\Models\Brand;
+use App\Models\ModelType;
 use App\Models\Product;
+use App\Models\Type;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
@@ -23,6 +26,30 @@ class ProductsImport implements ToCollection, WithHeadingRow
                     $product->increment('quantity', 1);
                 }
                 $product->save();  
+            } else {
+                if  ($row['status'] == 'Buy') {
+                    continue;
+                }
+                $type = Type::firstOrCreate(
+                    ['name' => $row['types']]
+                );
+
+                $brand = Brand::firstOrCreate(
+                    ['name' => $row['brand']]
+                );
+
+                $modeltype = ModelType::firstOrCreate(
+                    ['name' => $row['model'], 'capacity' => $row['capacity']],
+                    ['type_id' => $type->id, 'brand_id' => $brand->id]
+                );
+
+                $product = new Product;
+                $product->id = $row['product_id'];
+                $product->quantity = 1;
+                $product->type_id = $type->id;
+                $product->brand_id = $brand->id;
+                $product->model_type_id = $modeltype->id;
+                $product->save();
             }
         }
     }
